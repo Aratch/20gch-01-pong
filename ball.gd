686bc039e304fd712d6f899259e8c343d24d96bd
Andreas Ratchev <andreasratchev@gmail.com>
2025-02-10 12:49:03 +0100

    Ball: Center ball on start/reset

--- ball.gd
+++ ball.gd
@@ -9,6 +9,8 @@ var vis_notifier : VisibleOnScreenNotifier2D
 
 const START_DIRECTIONS := [Vector2.LEFT, Vector2.RIGHT]
 
+signal ball_exited
+
 func _set_up_setters():
 	collision_shape.changed.connect(func ():
 		var new_radius := collision_shape.radius
@@ -19,6 +21,15 @@ func _set_up_setters():
 		vis_notifier.position.y = -(new_radius/2)
 		)
 
+func _set_to_initial_pos() -> void:
+	var window_size := DisplayServer.window_get_size()
+	position.x = window_size.x / 2
+	position.y = window_size.y / 2
+
+func _start_game() -> void:
+	_set_to_initial_pos()
+	velocity = START_DIRECTIONS.pick_random()
+
 func _ready() -> void:
 	collision_shape = $CollisionShape2D.shape
 	ball_sprite = $BallSprite
@@ -26,10 +37,10 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		_set_up_setters()
 	else:
-		velocity = START_DIRECTIONS.pick_random()
 		vis_notifier.screen_exited.connect(func():
-			get_tree().reload_current_scene()
+			ball_exited.emit()
 			)
+		_start_game()
 	
 
 func _physics_process(delta: float) -> void:
