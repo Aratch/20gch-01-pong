27d85b20d2ee40816a3302388d5291c2fbdb2a66
Andreas Ratchev <andreasratchev@gmail.com>
2025-02-10 13:19:38 +0100

    Fix ball getting stuck as simply as possible

--- ball.gd
+++ ball.gd
@@ -24,8 +24,8 @@ func _set_up_setters():
 
 func _set_to_initial_pos() -> void:
 	var window_size := DisplayServer.window_get_size()
-	position.x = window_size.x / 2
-	position.y = window_size.y / 2
+	position.x = window_size.x / 2.0
+	position.y = window_size.y / 2.0
 
 func _start_game() -> void:
 	_set_to_initial_pos()
@@ -58,21 +58,6 @@ func _physics_process(delta: float) -> void:
 				velocity = (normal + 0.01 * collider_velocity).normalized()
 			else:
 				velocity = Vector2(velocity.x, -velocity.y)
-	
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
+		# Add some jitter if we're stuck
+		if collision.get_depth() > 0.3 and is_zero_approx(velocity.x):
+			velocity.x = randfn(0.0, 1.0)
--- paddle.gd
+++ paddle.gd
@@ -12,8 +12,8 @@ const JUMP_VELOCITY = -400.0
 
 func _start_game() -> void:
 	var window_size := DisplayServer.window_get_size()
-	position.y = window_size.y / 2
-	var ratio = 7
+	position.y = window_size.y / 2.0
+	var ratio = 7.0
 	if is_player:
 		position.x = window_size.x / ratio
 	else:
@@ -56,21 +56,3 @@ func _physics_process(delta: float) -> void:
 	var collision := move_and_collide(velocity * delta)
 	if collision:
 		pass # this works as expected
-	
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
